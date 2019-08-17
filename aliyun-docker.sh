d43af78c11236e0a0bac42fd334f222ff805482b
dh <441686961@qq.com>
2019-08-17 10:49:36 +0800

    docker

--- aliyun-docker.sh
+++ aliyun-docker.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-# docker login --username=dodofun2020 registry.cn-beijing.aliyuncs.com
-docker tag dodofun/vertx:latest registry.cn-beijing.aliyuncs.com/dodo-fun/vertx:latest
-docker push registry.cn-beijing.aliyuncs.com/dodo-fun/vertx:latest
 
+# docker 登录
+# docker login --username=dodofun2020 registry.cn-beijing.aliyuncs.com
+# 设立镜像
+docker tag dodofun/vertx:latest registry.cn-beijing.aliyuncs.com/dodo-fun/vertx:latest
+# 更新镜像
+docker push registry.cn-beijing.aliyuncs.com/dodo-fun/vertx:latest
+# 拉取最新镜像
 # docker pull registry.cn-beijing.aliyuncs.com/dodo-fun/vertx:latest
---
+++ dodofun-ssh/docker-start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# docker 安装
+# yum install docker
+# docker 启动
+systemctl daemon-reload
+systemctl restart docker
